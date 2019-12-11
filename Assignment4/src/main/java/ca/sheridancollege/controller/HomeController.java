7e57476d60a003a4eed665eb535187ab1ed3c34c
sanchebr <sanchebr@sheridancollege.ca>
2019-12-11 13:51:03 -0500

    hey alfred, I fixed your dumb program. You're welcome

--- Assignment4/src/main/java/ca/sheridancollege/controller/HomeController.java
+++ Assignment4/src/main/java/ca/sheridancollege/controller/HomeController.java
@@ -1,5 +1,14 @@
 package ca.sheridancollege.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+	
+	@GetMapping("/")
+	public String root() {
+		return "home.html";
+	}
 
 }
